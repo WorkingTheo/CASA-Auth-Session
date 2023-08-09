168d6b7597ed3d5b851ffe8919edefd41692e352
theo.selenskiy <theo.selenskiy@engineering.digital.dwp.gov.uk>
2023-08-09 15:47:46 +0100

    feat(logout): add a logout page

--- app/server.ts
+++ app/server.ts
@@ -79,7 +79,7 @@ expressApp.post('/message', (request: Request, response: Response, next: NextFun
 
   sendEventsToAll(message);
   response.sendStatus(200);
-})
+});
 
 const getStore = (store: string) => {
   if (store === 'authenticated') {
